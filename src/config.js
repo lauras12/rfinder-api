0310aedafea1950e232ee1c3c73cd824d97d58cc
Laura Sikora <lauraannsikora@gmail.com>
2021-03-17 18:13:42 -0400

    deploying for production

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL:process.env.DB_URL || 'postgresql://lsikora@localhost/rfinder-api',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://lsikora@localhost/rfinder-api-test',
+    DATABASE_URL:process.env.DATABASE_URL || 'postgresql://lsikora@localhost/rfinder-api',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL|| 'postgresql://lsikora@localhost/rfinder-api-test',
     JWT_SECRET: process.env.JWT_SECRET || 'finds',
+    CLIENT_ORIGIN: 'https://rfinder-app.now.sh/'
 }
