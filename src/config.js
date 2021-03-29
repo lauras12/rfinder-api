df3d193ee979c00987b97f6c35fcba30c6d4070d
Laura Sikora <lauraannsikora@gmail.com>
2021-03-29 15:51:28 -0400

    updated config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL:process.env.DATABASE_URL || 'postgresql://lsikora@localhost/rfinder-api',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL|| 'postgresql://lsikora@localhost/rfinder-api-test',
+    DATABASE_URL: (process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL),
     JWT_SECRET: process.env.JWT_SECRET || 'finds',
     CLIENT_ORIGIN: 'https://rfinder-app.vercel.app/'
 }
