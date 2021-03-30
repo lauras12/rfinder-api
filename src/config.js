d598053e8bd1eb64e06a123a1e8eb3168cb0d476
Laura Sikora <lauraannsikora@gmail.com>
2021-03-30 15:32:33 -0400

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     //DATABASE_URL:process.env.DATABASE_URL || 'postgresql://lsikora@localhost/rfinder-api',
@@ -7,4 +7,23 @@ module.exports = {
     DATABASE_URL:process.env.DATABASE_URL || 'postgresql://lsikora@localhost/rfinder-api',
     JWT_SECRET: process.env.JWT_SECRET || 'finds',
     CLIENT_ORIGIN: 'https://rfinder-app.vercel.app/'
+}*/
+
+/*
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL:process.env.DATABASE_URL || 'postgresql://lsikora@localhost/rfinder-api',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://lsikora@localhost/rfinder-api-test',
+    JWT_SECRET: process.env.JWT_SECRET || 'finds',
+    CLIENT_ORIGIN: 'https://rfinder-app.vercel.app/'
 }
+*/
+
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: (process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL),
+    JWT_SECRET: process.env.JWT_SECRET || 'finds',
+    CLIENT_ORIGIN: 'https://rfinder-app.vercel.app/'
+};
