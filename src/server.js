5123668ed98f4597468432790f970548907ddfce
Laura Sikora <lauraannsikora@gmail.com>
2021-04-05 18:45:11 -0400

    updates

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ pg.defaults.ssl = true;
 const app = require('./app')
 const knex =require('knex');
 const { PORT, DATABASE_URL } = require('./config');
+const parse = require('pg-connection-string').parse;
+const pgconfig = parse(DATABASE_URL);
+pgconfig.ssl = { rejectUnauthorized: false };
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connection: pgconfig
 })
 console.log(process.env.NODE_ENV)
 app.set('db', db);
