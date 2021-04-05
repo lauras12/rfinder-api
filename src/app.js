0bc0bf6cc584da05dbafc1dfefdc04c41f318047
Laura Sikora <lauraannsikora@gmail.com>
2021-04-05 17:07:28 -0400

    updated for app.js

--- src/app.js
+++ src/app.js
@@ -26,13 +26,13 @@ app.use(reviewsRouter);
 app.get('/', (req, res) => {
     res.send('hello from restaurant finder!')
 });
-// app.use(function(req, res, next) {
-//     // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Origin", "https://rfinder-app.vercel.app/");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
+/*
+app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+     res.header("Access-Control-Allow-Origin", "https://rfinder-app.vercel.app/");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");     
+     next();   });
+*/
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
