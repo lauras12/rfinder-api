00dea192585310c2bdaf3e7f1e171c59bd30898e
Laura Sikora <lauraannsikora@gmail.com>
2021-04-07 09:54:17 -0400

    add pics to readme

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -13,19 +14,19 @@ const placesRouter = require('./places/places-router');
 const reviewsRouter = require('./reviews/reviews-router');
 const {CLIENT_ORIGIN} = require('./config');
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
+    app.use(morgan(morganOption));
+    app.use(helmet());
+    app.use(cors());
 
-app.use(proxyRouter);
-app.use(authRouter);
-app.use(userRouter);
-app.use(placesRouter);
-app.use(reviewsRouter);
+    app.use(proxyRouter);
+    app.use(authRouter);
+    app.use(userRouter);
+    app.use(placesRouter);
+    app.use(reviewsRouter);
 
-app.get('/', (req, res) => {
-    res.send('hello from restaurant finder!')
-});
+    app.get('/', (req, res) => {
+        res.send('hello from restaurant finder!')
+    });
 /*
 app.use(function(req, res, next) {
      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -33,17 +34,17 @@ app.use(function(req, res, next) {
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");     
      next();   });
 */
-app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-        console.log(error);
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-});
+    app.use(function errorHandler(error, req, res, next) {
+        let response;
+        if (NODE_ENV === 'production') {
+            response = { error: { message: 'server error' } }
+            console.log(error);
+        } else {
+            console.error(error)
+            response = { message: error.message, error }
+        }
+        res.status(500).json(response)
+    });
 
 module.exports = app;
 
